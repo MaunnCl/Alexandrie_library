60e1bdea79034a1b7923fc879625a546929d00af
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-01-30 15:57:58 +0100

    feat(Backend): add User model with Sequelize integration

---
+++ Backend/config/models/user.model.ts
@@ -0,0 +1,44 @@
+import sequelize from "../database";
+import { DataTypes, Model } from "sequelize";
+
+class User extends Model {
+    public id!: number;
+    public email!: string;
+    public password!: string;
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    }
+}, {
+    sequelize,
+    modelName: "User"
+}
+)
+
+export default User;
