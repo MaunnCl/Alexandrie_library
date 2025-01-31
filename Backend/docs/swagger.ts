bad98fc1a972f9c82a902223f262174b069e86d3
Mathis Champin <mathis.champin@epitech.eu>
2025-01-31 11:16:11 +0100

    feat(Backend): add swagger for routes

---
+++ Backend/docs/swagger.ts
@@ -0,0 +1,22 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+
+const options: swaggerJSDoc.Options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API Documentation",
+            version: "1.0.0",
+            description: "Documentation de l'API avec Swagger",
+        },
+        servers: [{ url: "http://localhost:8080/api" }],
+    },
+    apis: ["./routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export const setupSwagger = (app: Express) => {
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
--- Backend/server.ts
+++ Backend/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route";
 import authRoutes from "./routes/auth.route";
+import { setupSwagger } from "./docs/swagger";
 
 dotenv.config();
 
@@ -12,6 +13,10 @@ app.use(express.json());
 app.use("/api", userRoutes);
 app.use("/api", authRoutes);
 
+// Ajout de Swagger
+setupSwagger(app);
+
 app.listen(PORT, () => {
     console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`📄 Swagger disponible sur http://localhost:${PORT}/api-docs`);
 });
