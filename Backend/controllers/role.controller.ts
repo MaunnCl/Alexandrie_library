a47cd324d52ffd26eeed21fdfbac236eb2bab017
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-02-20 13:53:59 +0100

    fix(Backend): ensure role ID is parsed as an integer in role controller methods

--- Backend/controllers/role.controller.ts
+++ Backend/controllers/role.controller.ts
@@ -21,8 +21,10 @@ export class RoleController {
     }
 
     static async getRoleById(req: Request, res: Response) {
+        let id =  parseInt(req.params.id);
+        if (!id) return res.status(400).json({ message: "ID du rôle manquant" });
         try {
-            const role = await RoleService.getRoleById(req.params.id);
+            const role = await RoleService.getRoleById(id);
             if (!role) return res.status(404).json({ message: "Rôle non trouvé" });
             res.status(200).json(role);
         } catch (error) {
@@ -32,7 +34,7 @@ export class RoleController {
 
     static async updateRole(req: Request, res: Response) {
         try {
-            const role = await RoleService.updateRole(req.params.id, req.body);
+            const role = await RoleService.updateRole(parseInt(req.params.id), req.body);
             res.status(200).json(role);
         } catch (error) {
             res.status(500).json({ message: "Erreur lors de la mise à jour du rôle", error });
@@ -41,7 +43,7 @@ export class RoleController {
 
     static async deleteRole(req: Request, res: Response) {
         try {
-            await RoleService.deleteRole(req.params.id);
+            await RoleService.deleteRole(parseInt(req.params.id));
             res.status(200).json({ message: "Rôle supprimé avec succès" });
         } catch (error) {
             res.status(500).json({ message: "Erreur lors de la suppression du rôle", error });
