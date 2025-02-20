b60ef69215effa45cdd779ffb763e0bd6d316620
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-02-20 15:01:23 +0100

    feat(Backend): add video controller, service, and routes for video management

---
+++ Backend/controllers/video.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+class VideoController {
+    static async getVideos(req: Request, res: Response) {}
+    static async getVideoById(req: Request, res: Response) {}
+    static async uploadVideo(req: Request, res: Response) {}
+    static async streamVideo(req: Request, res: Response) {}
+}
