05a0f72868c09087a558b00baac43f21177846b8
rafael-sapalo <rafael.sapalo-esteves@epitech.eu>
2025-02-27 16:26:13 +0100

    fix(Backend): update import paths in server.ts to use TypeScript extensions and adjust API routing feat(Frontend): add Dockerfile for frontend application setup and configuration

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,11 +1,10 @@
-import router from "../Backend/routes/user.route";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import authRoutes from "./routes/auth.route";
-import userRoutes from "./routes/user.route";
-import userProfileRoutes from "./routes/userProfile.route";
-import roleRoutes from "./routes/roleList.route";
+import authRoutes from "routes/auth.route.ts";
+import userRoutes from "routes/user.route.ts";
+import userProfileRoutes from "routes/userProfile.route.ts";
+import roleRoutes from "routes/roleList.route";
 import usersRolesRoutes from "./routes/userRole.route";
 import contentRoutes from "./routes/content.route";
 import historyRoutes from "./routes/history.route";
@@ -19,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors())
-app.use("/api", router);
+app.use("/api", userRoutes);
 app.use((req: express.Request, res, next) =>{
     const time = new Date(Date.now()).toString();
     console.log(req.method,req.hostname, req.path, time);
