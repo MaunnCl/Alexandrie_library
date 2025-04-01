a656158218ada4e90e640e09d827bca89e1d2301
Mathis Champin <mathis.champin@epitech.eu>
2025-04-01 15:28:04 +0200

    feat(Backend): add routes for search url of video with title of them in aws

--- Backend/services/content.service.ts
+++ Backend/services/content.service.ts
@@ -1,23 +1,32 @@
 import { ContentRepository } from "../repository/content.repository";
+import {
+  getSignedUrlForStreaming,
+  buildS3KeyFromTitle,
+} from "../utils/aws.utils";
 
 export class ContentService {
-    static async createContent(data: any) {
-        return ContentRepository.create(data);
-    }
+  static async createContent(data: any) {
+    const videoKey = buildS3KeyFromTitle(data.title);
+    const signedUrl = await getSignedUrlForStreaming(videoKey);
+    return await ContentRepository.create({
+      ...data,
+      url: signedUrl,
+    });
+  }
 
-    static async getAllContents() {
-        return ContentRepository.findAll();
-    }
+  static async getAllContents() {
+    return await ContentRepository.findAll();
+  }
 
-    static async getContentById(id: number) {
-        return ContentRepository.findById(id);
-    }
+  static async getContentById(id: number) {
+    return await ContentRepository.findById(id);
+  }
 
-    static async updateContent(id: number, data: any) {
-        return ContentRepository.update(id, data);
-    }
+  static async updateContent(id: number, data: any) {
+    return await ContentRepository.update(id, data);
+  }
 
-    static async deleteContent(id: number) {
-        return ContentRepository.delete(id);
-    }
+  static async deleteContent(id: number) {
+    return await ContentRepository.delete(id);
+  }
 }
