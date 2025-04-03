33ec9bcde0395215526a3a658a3c02915b91d034
username <damian.gil@epitech.eu>
2025-04-03 15:45:48 +0200

    fix(Backend): update server port to 5863 and adjust Nginx proxy_pass configuration

--- Backend/server.ts
+++ Backend/server.ts
@@ -14,7 +14,7 @@ import { setupSwagger } from "./docs/swagger";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5863;
 
 app.use(express.json());
 app.use(cors())
