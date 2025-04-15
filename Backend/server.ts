4fc86145d20ab2ccf9d36cf0d7f2fdcdec0ddc60
Mathis Champin <mathis.champin@epitech.eu>
2025-04-15 13:29:09 +0200

    Add package.json for CI

--- Backend/server.ts
+++ Backend/server.ts
@@ -12,9 +12,7 @@ import congressRoutes from "./routes/congress.route";
 import sessionRoutes from "./routes/session.route";
 import { setupSwagger } from "./docs/swagger";
 
-dotenv.config({
-    path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
-  });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
